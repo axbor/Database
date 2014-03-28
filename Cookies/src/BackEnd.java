adf7f974d6d9796e280205f571ee48533f8ebb50
Axel Borgdén <axel.borgden@gmail.com>
2014-03-28 11:08:37 +0100

    fix med backend

--- Cookies/src/BackEnd.java
+++ Cookies/src/BackEnd.java
@@ -1,6 +1,10 @@
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Vector;
+
+import javax.print.DocFlavor.READER;
 
 
 
@@ -12,13 +16,32 @@ public class BackEnd {
 	private Connection conn;
 	private String createPalletQuery;
 	private String getNbrOfBatchesQuery;
+	private String createBatchQuery;
+	private String getIngredientAmountRecipeQuery;
+	private String getIngredientsQuery;
+	private String updateMaterialQuery;
+	private String getMaterialAmountQuery;
+	private String getBatchInfoQuery;
+	private String getPalletInfoQuery;
+	private String getBatchesQuery;
+	private String getCookiesQuery;
+	private String blockBatchQuery;
 
 	public BackEnd(){
 		conn = null;
 		
-		createPalletQuery = "Insert into Pallets values (null,?,created)";
-		getNbrOfBatchesQuery = "select ";
-		
+//		createPalletQuery = "Insert into Pallets values (null,?,created)";
+//		getNbrOfBatchesQuery = "select ";
+		createBatchQuery = "insert into ProdictionBatch values(null, ?, now(), Untested)";
+		getIngredientAmountRecipeQuery = "select amount from RawMaterial where ingredientName = ?";
+		getIngredientsQuery = "select ingredientName from CookieContains where cookieName = ?";
+		updateMaterialQuery = "update RawMaterial set amount = amount - ? where ingredientName = ?";
+		getMaterialAmountQuery = "select amount from RawMaterial where ingredientName = ?";
+		getBatchInfoQuery = "select cookieName, prodDate, QA from ProductionBatch where batchNumber = ?";
+		getPalletInfoQuery = "select orderNumber, status from Pallet where palletNumber = ?";
+		getBatchesQuery = "select batchNumber, cookieName from ProductionBatch";
+		getCookiesQuery = "select cookieName from Recipe";
+		blockBatchQuery = "update productionBatch set QA = 'blocked' where batchNumber = ?";
 	}
 	
 	public boolean openConnection() {
@@ -77,15 +100,85 @@ public class BackEnd {
 	}
 
 	
-	public void createBatch(String recipe){
-
-		return;
+	public boolean createBatch(String cookieName){
+		PreparedStatement createBatch;
+		PreparedStatement getIngredientAmount;
+		ResultSet amountSet;
+		ArrayList<String> ingredientList = getIngredients(cookieName);
+		HashMap<String ,Double> amountExists = new HashMap<String, Double>();
+		try {
+			for(int i = 0; i < ingredientList.size(); i ++) {
+				getIngredientAmount = conn.prepareStatement(getIngredientAmountRecipeQuery);
+				getIngredientAmount.setString(1, ingredientList.get(i));
+				amountSet = getIngredientAmount.executeQuery();
+				amountSet.next();
+				double amount = amountSet.getDouble("amount");
+				if (amount < requiredAmount(cookieName) || requiredAmount(cookieName) == -1){
+					return false;
+				}else {
+					amountExists.put(ingredientList.get(i), amount);					
+				}
+			}
+			for(int i = 0; i < ingredientList.size(); i ++) {
+				double amount = amountExists.get(i);
+				updateRawMaterial(ingredientList.get(i), amount);
+			}
+		}catch(SQLException e) {
+			System.err.println(e);
+			return false;
+		}
+		return true;
 		
 	}
 	
-	public void updateRawMaterial(String recipe, int pallets){
-		
-		return;
+	private double requiredAmount(String cookieName) {
+		PreparedStatement getAmount;
+		ResultSet amountSet;
+		double amount = -1;
+		try {
+			getAmount = conn.prepareStatement(getMaterialAmountQuery);
+			getAmount.setString(1, cookieName);
+			
+			amountSet = getAmount.executeQuery();
+			amountSet.next();
+			amount = amountSet.getDouble("amount");
+		} catch(SQLException e) {
+			System.err.println(e);
+		}
+		return amount;
+	}
+
+	public ArrayList<String> getIngredients(String cookieName) {
+		PreparedStatement getIngredients;
+		ArrayList<String> ingredientList = new ArrayList<String>();
+		ResultSet ingredients;
+		try{
+			getIngredients = conn.prepareStatement(getIngredientsQuery);
+			getIngredients.setString(1, cookieName);
+			
+			ingredients = getIngredients.executeQuery();
+			
+			while(ingredients.next()){
+				ingredientList.add(ingredients.getString("ingredientName"));
+			}	
+		}catch(SQLException e) {
+			System.err.println(e);
+			return null;
+		}
+		return ingredientList;
+	}
+	
+	public void updateRawMaterial(String ingredientName, double amount){
+		PreparedStatement updateMaterial;
+		try{
+			updateMaterial = conn.prepareStatement(updateMaterialQuery);
+			updateMaterial.setDouble(1, amount);
+			updateMaterial.setString(2, ingredientName);
+			
+			updateMaterial.execute();
+		}catch(SQLException e) {
+			System.err.println(e);
+		}
 	}
 	
 	public void createPallet(String recipe, int nbrOfPallets){
@@ -94,9 +187,88 @@ public class BackEnd {
 		return;
 	}
 
-	private int nbrOfBatches(String recipe) {
+
+
+	public String getBatchInfo(int batchNbr) {
+		PreparedStatement getBatchInfo;
+		ResultSet batchInfoSet;
+		StringBuilder sb = new StringBuilder();
+		try {
+			getBatchInfo = conn.prepareStatement(getBatchInfoQuery);
+			getBatchInfo.setInt(1, batchNbr);
+			
+			batchInfoSet = getBatchInfo.executeQuery();
+			batchInfoSet.next();
+			sb.append("Batch number" + batchNbr + " has ");
+			sb.append(batchInfoSet.getString("cookieName") + " and was produced on ");
+			sb.append(batchInfoSet.getString("prodDate") + " and has the QA result: ");
+			sb.append(batchInfoSet.getString("QA"));
+		} catch(SQLException e) {
+			System.err.println(e);
+		}
+		return sb.toString();
+	}
+
+	public String getPalletInfo(int palletNbr) {
+		PreparedStatement getPalletInfo;
+		ResultSet palletInfoSet;
+		StringBuilder sb = new StringBuilder();
+		try {
+			getPalletInfo = conn.prepareStatement(getPalletInfoQuery);
+			getPalletInfo.setInt(1, palletNbr);
+			
+			palletInfoSet = getPalletInfo.executeQuery();
+			palletInfoSet.next();
+			sb.append("Pallet number" + palletNbr + " belongs to order number ");
+			sb.append(palletInfoSet.getString("orderNumber") + " and has status: ");
+			sb.append(palletInfoSet.getString("status"));
+		} catch(SQLException e) {
+			System.err.println(e);
+		}
+		return sb.toString();
+	}
+
+	public boolean blockBatch(int batchNbr) {
+		PreparedStatement blockBatches;
+//		ResultSet
 		
-		return 0;
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public ArrayList<String> getBatches() {
+		PreparedStatement getBatches;
+		ResultSet batchSet;
+		ArrayList<String> batches = new ArrayList<String>();
+		try {
+			getBatches = conn.prepareStatement(getBatchesQuery);
+			batchSet = getBatches.executeQuery();
+			while(batchSet.next()) {
+				StringBuilder sb = new StringBuilder();
+				sb.append(batchSet.getInt("batchNumber") + " -- " + batchSet.getString("cookieName"));
+				batches.add(sb.toString());
+			}
+		}catch(SQLException e) {
+			System.err.println(e);
+		}
+		return batches;
+	}
+
+	public ArrayList<String> getCookies() {
+		PreparedStatement getCookies;
+		ResultSet cookieSet;
+		ArrayList<String> cookies = new ArrayList<String>();
+		try {
+			getCookies = conn.prepareStatement(getCookiesQuery);
+			cookieSet = getCookies.executeQuery();
+			while(cookieSet.next()) {
+				StringBuilder sb = new StringBuilder();
+				sb.append(cookieSet.getString("cookieName"));
+				cookies.add(sb.toString());
+			}
+		}catch(SQLException e) {
+			System.err.println(e);
+		}
+		return cookies;
 	}
 }
-		
--- Cookies/src/GUI.java
+++ Cookies/src/GUI.java
@@ -15,7 +15,6 @@ public class GUI {
 	/**
 	 * db is the database object
 	 */
-	private Database db;
 
 	/**
 	 * tabbedPane is the contents of the window. It consists of two panes, User
@@ -36,31 +35,31 @@ public class GUI {
 	 *            The database.
 	 * @wbp.parser.entryPoint
 	 */
-	public GUI(final BackEnd be) {
-		
+	public GUI(BackEnd b) {
+		final BackEnd be = b;
 		JFrame frame = new JFrame("Cookies");	
 		frame.setSize(500,500);
-		
+
 		//create Tab /////////////////////////
-		
+
 		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
 		frame.getContentPane().add(tabbedPane_1, BorderLayout.CENTER);
-		
+
 		JPanel createPanel = new JPanel();
 		tabbedPane_1.addTab("Create", null, createPanel, null);
 		createPanel.setLayout(null);
-				
+
 		list = new JList<String>();
-		list.setModel(new CookieListModel(db));
+		list.setModel(new CookieListModel(be));
 		list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
 		createPanel.add(list);
 		list.setBounds(50, 94, 150, 200);
-		
+
 		cookieAmountField = new JTextField();
 		cookieAmountField.setBounds(284, 106, 144, 62);
 		createPanel.add(cookieAmountField);
 		cookieAmountField.setColumns(10);
-		
+
 		JButton btnOk = new JButton("OK");
 		btnOk.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -74,6 +73,7 @@ public class GUI {
 						ListModel<String> model = list.getModel();
 						String cookie = model.getElementAt(index);
 						cookieAmountField.setText(cookie);
+						//TODO: implementera den nedanför
 						be.createPallet(cookie, amount);
 					}
 				}catch(NumberFormatException err) {
@@ -83,30 +83,30 @@ public class GUI {
 		});
 		btnOk.setBounds(284, 291, 117, 25);
 		createPanel.add(btnOk);
-		
+
 		JLabel lblAmountOfPallets = new JLabel("Amount of Pallets");
 		lblAmountOfPallets.setBounds(284, 79, 136, 15);
 		createPanel.add(lblAmountOfPallets);
-		
+
 		JLabel lblChooseACookie = new JLabel("Choose a cookie type to create");
 		lblChooseACookie.setBounds(33, 51, 221, 15);
 		createPanel.add(lblChooseACookie);
 
 		//Search Pallet tab////////////////////////////
-		
+
 		JPanel searchPalletPanel = new JPanel();
 		tabbedPane_1.addTab("Search Pallet", null, searchPalletPanel, null);
 		searchPalletPanel.setLayout(null);
-		
+
 		palletNumberField = new JTextField();
 		palletNumberField.setBounds(125, 106, 213, 82);
 		searchPalletPanel.add(palletNumberField);
 		palletNumberField.setColumns(10);
-		
+
 		JLabel lblWritePalletNumber = new JLabel("Write pallet number of the pallet you want to search for");
 		lblWritePalletNumber.setBounds(55, 68, 428, 15);
 		searchPalletPanel.add(lblWritePalletNumber);
-		
+
 		JButton btnOk_1 = new JButton("Ok");
 		btnOk_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -116,8 +116,9 @@ public class GUI {
 					if(palletNbr < 1) {
 						JOptionPane.showMessageDialog(null, "The pallet number has to be bigger than 0");
 					}else {
-//						String info = db.getPalletInfo(palletNbr);
-//						palletInfoField.setText(info);
+						//TODO: implementera den nadanför
+						String info = be.getPalletInfo(palletNbr);
+						//						palletInfoField.setText(info);
 						palletInfoField.setText("hej");
 					}
 				}catch(NumberFormatException err) {
@@ -127,37 +128,37 @@ public class GUI {
 		});
 		btnOk_1.setBounds(172, 200, 117, 25);
 		searchPalletPanel.add(btnOk_1);
-		
+
 		palletInfoField = new JTextField();
 		palletInfoField.setEditable(false);
 		palletInfoField.setBounds(55, 279, 386, 131);
 		searchPalletPanel.add(palletInfoField);
 		palletInfoField.setColumns(10);
-		
+
 		//Hämta info från pallen
-		
-		
+
+
 		JLabel lblInfo = new JLabel("Info");
 		lblInfo.setBounds(56, 233, 70, 15);
 		searchPalletPanel.add(lblInfo);
-		
-		
+
+
 
 		//Search Batch tab////////////////////////////
-		
+
 		JPanel searchBatchPanel = new JPanel();
 		tabbedPane_1.addTab("Search Batch", null, searchBatchPanel, null);
 		searchBatchPanel.setLayout(null);
-		
+
 		batchNumberField = new JTextField();
 		batchNumberField.setBounds(125, 106, 213, 82);
 		searchBatchPanel.add(batchNumberField);
 		batchNumberField.setColumns(10);
-		
+
 		JLabel lblWriteBatchNumber = new JLabel("Write batch number of the batch you want to search for");
 		lblWriteBatchNumber.setBounds(55, 68, 428, 15);
 		searchBatchPanel.add(lblWriteBatchNumber);
-		
+
 		JButton btnOk_2 = new JButton("Ok");
 		btnOk_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -167,9 +168,10 @@ public class GUI {
 					if(batchNbr < 1) {
 						JOptionPane.showMessageDialog(null, "The batch number has to be bigger than 0");
 					}else {
-//						String info = db.getBatchInfo(batchNbr);
-//						palletInfoField.setText(info);
-						palletInfoField.setText("hej");
+						//TODO: implementera den nadanför
+						String info = be.getBatchInfo(batchNbr);
+						palletInfoField.setText(info);
+						//						palletInfoField.setText("hej");
 					}
 				}catch(NumberFormatException err) {
 					JOptionPane.showMessageDialog(null, "The batch number is an integer bigger than 0");
@@ -178,44 +180,45 @@ public class GUI {
 		});
 		btnOk_2.setBounds(172, 200, 117, 25);
 		searchBatchPanel.add(btnOk_2);
-		
+
 		batchInfoField = new JTextField();
 		batchInfoField.setEditable(false);
 		batchInfoField.setBounds(55, 279, 386, 131);
 		searchBatchPanel.add(batchInfoField);
 		batchInfoField.setColumns(10);
-		
+
 		//Hämta info från batchen
-		
-		
+
+
 		JLabel lblBatchInfo = new JLabel("Info");
 		lblBatchInfo.setBounds(56, 233, 70, 15);
 		searchBatchPanel.add(lblBatchInfo);
-		
-		
-		
-		
+
+
+
+
 		//blocking panel /////////////////////////////////
-		
-		
+
+
 		JPanel blockingPanel = new JPanel();
 		tabbedPane_1.addTab("Blocking", null, blockingPanel, null);
 		blockingPanel.setLayout(null);
-		
+
 		final JList blockList = new JList();
 		blockList.setBounds(45, 47, 256, 385);
-		blockList.setModel(new BlockingListModel(db));
+		blockList.setModel(new BlockingListModel(be));
 		blockList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
 		blockingPanel.add(blockList);
-		
+
 		JButton btnBlock = new JButton("Block");
 		btnBlock.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try{
-						int index = blockList.getSelectedIndex();
-						ListModel<String> model = blockList.getModel();
-						int batch= Integer.parseInt(model.getElementAt(index));
-//						db.blockBatch(batch);
+					int index = blockList.getSelectedIndex();
+					ListModel<String> model = blockList.getModel();
+					int batchNbr= Integer.parseInt(model.getElementAt(index));
+					//TODO: implementera den nadanför
+					be.blockBatch(batchNbr);
 				}catch(NumberFormatException err) {
 					JOptionPane.showMessageDialog(null, "The batch number is an integer bigger than 0");
 				}
@@ -223,57 +226,57 @@ public class GUI {
 		});
 		btnBlock.setBounds(398, 228, 71, 25);
 		blockingPanel.add(btnBlock);
-		
-		
-		
+
+
+
 		JLabel lblNewLabel = new JLabel("Block Batch");
 		lblNewLabel.setBounds(45, 12, 101, 15);
 		blockingPanel.add(lblNewLabel);
-		
 
-		
+
+
 		frame.setVisible(true);
 	}
-	
+
 	class CookieListModel extends AbstractListModel<String> {
 		private ArrayList<String> cookies = new ArrayList<String>();
-		
-	    public CookieListModel(Database db){
-//	    	cookies = db.getCookies();
-	        cookies.add("banankaka");
-	        cookies.add("sockarkaka");
-	        cookies.add("pepparkaka");
-	        }
 
-	        @Override
-	        public int getSize() {
-	        return cookies.size();
-	        }
+		public CookieListModel(BackEnd be){
+			cookies = be.getCookies();
+//			cookies.add("banankaka");
+//			cookies.add("sockarkaka");
+//			cookies.add("pepparkaka");
+		}
 
-	        @Override
-	        public String getElementAt(int index) {
-	        return cookies.get(index);
-	        }
+		@Override
+		public int getSize() {
+			return cookies.size();
+		}
+
+		@Override
+		public String getElementAt(int index) {
+			return cookies.get(index);
+		}
 	}
-	
+
 	class BlockingListModel extends AbstractListModel<String> {
 		private ArrayList<String> blocking = new ArrayList<String>();
-		
-	    public BlockingListModel(Database db){
-//	    	blocking = db.getBatches();
-	        blocking.add("1");
-	        blocking.add("2");
-	        blocking.add("3");
-	        }
 
-	        @Override
-	        public int getSize() {
-	        return blocking.size();
-	        }
+		public BlockingListModel(BackEnd be){
+			blocking = be.getBatches();
+//			blocking.add("1");
+//			blocking.add("2");
+//			blocking.add("3");
+		}
 
-	        @Override
-	        public String getElementAt(int index) {
-	        return blocking.get(index);
-	        }
+		@Override
+		public int getSize() {
+			return blocking.size();
+		}
+
+		@Override
+		public String getElementAt(int index) {
+			return blocking.get(index);
+		}
 	}
 }
