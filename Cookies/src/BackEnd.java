19d273672362f76c1b265c8cdfef5f7ab0468652
Martin Lööf <mrtn.loof@gmail.com>
2014-03-29 16:44:13 +0100

    fixed search by status

--- Cookies/src/BackEnd.java
+++ Cookies/src/BackEnd.java
@@ -50,7 +50,7 @@ public class BackEnd {
 		getBlockedPalletsQuery = "select palletNumber from PalletsInBatch where batchNumber = ?";
 		setBlockedPalletsQuery = "update Pallet set status = 'blocked' where palletNumber = ?";
 		movePalletQuery = "update Pallets set status = 'in storage' where palletNumber = ?";
-		searchByStatusQuery = "select batchNumber, palletNumber, cookieName from Pallet natural join PalletsInBatch natural join productionBatch where status = ?";
+		searchByStatusQuery = "select batchNumber, palletNumber, cookieName from Pallet natural join PalletsInBatch natural join productionBatch where status = ? order by batchNumber, palletNumber";
 
 
 	}
